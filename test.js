2a94f3f7a505cca4daf4e2bc04b0604e34118752
JodMinster <jodminster@gmx.net>
2018-10-20 22:38:17 +0200

    correct

--- test.js
+++
@@ -1,5 +0,0 @@
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-const http = require('http')
-
-http.createServer((req, res) => res.end('test')).listen(port, ip, e => console.log('listening to ' + ip + ':' + port))
